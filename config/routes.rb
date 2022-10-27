810a645f363380cafc67b5d2f2a5a4d86512ad94
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-10-27 17:09:52 +0200

    Add footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   resources :symptoms do
     resources :medications
     resources :moods
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'symptoms#index'
+  root 'pages#index'
 end
