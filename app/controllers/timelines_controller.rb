a59958317cd436e7ad1d4ccc7088546f44549f73
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-11-04 17:49:46 +0200

    Add cancel button

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,6 @@
 class TimelinesController < ApplicationController
   before_action :authenticate_user!
   def index
-    render "symptoms/index"
+    render partial: 'symptoms/symptom', locals: { symptoms: @symptoms }
   end
 end
