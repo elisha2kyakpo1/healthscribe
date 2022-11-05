7290621a2e015d79bf5568c2ade0b6ad2c371b05
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-11-05 19:57:22 +0200

    Add styling to the forms

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,5 @@
 class TimelinesController < ApplicationController
   before_action :authenticate_user!
   def index
-    render partial: 'symptoms/symptom', locals: { symptoms: @symptoms }
   end
 end
