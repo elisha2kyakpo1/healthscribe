a2b5ca12008f9359c67e04041938d83995aea642
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-11-07 11:02:03 +0200

    Add error message to sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :pic) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :password_confirmation, :pic) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:full_name, :email, :password, :current_password, :pic)
     end
