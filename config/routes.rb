2557ef839360c3aac3d2d99108d8442419782766
Elisha kyakopo <kyakopoe@gmail.com>
2024-01-13 14:57:15 +0200

    Change url paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'entries/index'
+  get '/entries', to: 'entries#index'
   get 'pages/index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get '/timeline', to: 'users#timeline', as: 'timeline'
-  get '/analysis', to: 'users#insight', as: 'symptom_analysis'
+  get '/symptoms_analysis', to: 'users#insight', as: 'symptom_analysis'
   get '/popup', to: 'users#popup'
   get '/hovercard', to: 'users#hovercard', as: 'hovercard'
   resources :symptoms
